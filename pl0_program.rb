ab3e91157c9a683148a77134c8e90a5d5ffce5e7
juanma3108 <alu0100597929@ull.edu.es>
2014-04-22 22:38:21 +0100

    cambio en la visualizacion de archivos guardados

--- pl0_program.rb
+++ pl0_program.rb
@@ -8,6 +8,8 @@ class PL0Program
   
   property :name, String, :key => true
   property :source, String, :length => 1..1024
+  property :user, String, :length => 1..1024
+  property :provider, String, :length => 1..1024
 end
 
 DataMapper.finalize
